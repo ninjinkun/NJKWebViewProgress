7ec1782bcdee2022a1603a6ee1bc605161fa4dff
ninjin <ninjin@mac.com>
2013-11-16 12:11:16 +0900

    Update DemoApp using NJKWebviewProgressView

--- DemoApp/ViewController.m
+++ DemoApp/ViewController.m
@@ -7,11 +7,11 @@
 //
 
 #import "ViewController.h"
-
+#import "NJKWebViewProgressView.h"
 @implementation ViewController
 {
     IBOutlet __weak UIWebView *_webView;
-    IBOutlet __weak UIProgressView *_progressView;
+    NJKWebViewProgressView *_progressView;
     NJKWebViewProgress *_progressProxy;
 }
 
@@ -24,7 +24,27 @@
     _progressProxy.webViewProxyDelegate = self;
     _progressProxy.progressDelegate = self;
 
+    CGFloat progressBarHeight = 2.5f;
+    CGRect navigaitonBarBounds = self.navigationController.navigationBar.bounds;
+    CGRect barFrame = CGRectMake(0, navigaitonBarBounds.size.height - progressBarHeight, navigaitonBarBounds.size.width, progressBarHeight);
+    _progressView = [[NJKWebViewProgressView alloc] initWithFrame:barFrame];
+
     [self loadGoogle];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController.navigationBar addSubview:_progressView];
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+
+    // Remove progress view
+    // because UINavigationBar is shared with other ViewControllers
+    [_progressView removeFromSuperview];
 }
 
 - (IBAction)searchButtonPushed:(id)sender
@@ -39,28 +59,15 @@
 
 -(void)loadGoogle
 {
-    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://google.com/"]];
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://google.com/"]];
     [_webView loadRequest:req];
 }
 
 #pragma mark - NJKWebViewProgressDelegate
 -(void)webViewProgress:(NJKWebViewProgress *)webViewProgress updateProgress:(float)progress
 {
-    if (progress == 0.0) {
-        [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
-        _progressView.progress = 0;
-        [UIView animateWithDuration:0.27 animations:^{
-            _progressView.alpha = 1.0;
-        }];
-    }
-    if (progress == 1.0) {
-        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-        [UIView animateWithDuration:0.27 delay:progress - _progressView.progress options:0 animations:^{
-            _progressView.alpha = 0.0;
-        } completion:nil];
-    }
-        
-    [_progressView setProgress:progress animated:NO];
+    [_progressView setProgress:progress animated:YES];
+    self.title = [_webView stringByEvaluatingJavaScriptFromString:@"document.title"];
 }
 
 @end
