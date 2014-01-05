3f48962eab3b4c106bb76341d3d1555f304c5bee
Christian Roman <chroman16@gmail.com>
2014-01-04 23:35:24 -0600

    Global tintColor

--- NJKWebViewProgress/NJKWebViewProgressView.m
+++ NJKWebViewProgress/NJKWebViewProgressView.m
@@ -17,7 +17,11 @@
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         _progressBarView = [[UIView alloc] initWithFrame:self.bounds];
         _progressBarView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-        _progressBarView.backgroundColor = [UIColor colorWithRed:22.f / 255.f green:126.f / 255.f blue:251.f / 255.f alpha:1.0]; // iOS7 Safari bar color
+        UIColor *tintColor = [UIColor colorWithRed:22.f / 255.f green:126.f / 255.f blue:251.f / 255.f alpha:1.0]; // iOS7 Safari bar color
+        if ([UIApplication.sharedApplication.delegate.window respondsToSelector:@selector(setTintColor:)]) {
+            tintColor = UIApplication.sharedApplication.delegate.window.tintColor;
+        }
+        _progressBarView.backgroundColor = tintColor;
         [self addSubview:_progressBarView];
 
         _barAnimationDuration = 0.27f;
