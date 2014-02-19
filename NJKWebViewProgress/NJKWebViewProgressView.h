63c199ec945fc4071bde30af17ccf6ab01b325d4
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-02-19 23:05:46 +0000

    Update NJKWebViewProgressView.h
    add progress bar color property

--- NJKWebViewProgress/NJKWebViewProgressView.h
+++ NJKWebViewProgress/NJKWebViewProgressView.h
@@ -12,6 +12,7 @@
 @property (nonatomic) float progress;
 
 @property (nonatomic) UIView *progressBarView;
+@property (nonatomic) UIColor *progressBarColor; // default is window tint color
 @property (nonatomic) NSTimeInterval barAnimationDuration; // default 0.1
 @property (nonatomic) NSTimeInterval fadeAnimationDuration; // default 0.27
 @property (nonatomic) NSTimeInterval fadeOutDelay; // default 0.1
