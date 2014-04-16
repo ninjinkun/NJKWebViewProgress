81af0faffd9dd7d2ac45821ac6ebeb20a6e5ee1a
Lukman Sanusi <larryryu@gmail.com>
2014-04-15 23:23:47 -0500

    added UIViewAutoresizingFlexibleHeight to _progressBarView autoresizingMask. This ensures that it resizes with its NJKWebViewProgressView containerView. (When using constraints based layout the frame for NJKWebViewProgressView may initially be CGRectZero and the correct frame may be set afterwards, but the height of the of the _progressBarView would still be zero therefore invisible.)

--- NJKWebViewProgress/NJKWebViewProgressView.m
+++ NJKWebViewProgress/NJKWebViewProgressView.m
@@ -16,7 +16,7 @@
         self.userInteractionEnabled = NO;
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         _progressBarView = [[UIView alloc] initWithFrame:self.bounds];
-        _progressBarView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+        _progressBarView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
         UIColor *tintColor = [UIColor colorWithRed:22.f / 255.f green:126.f / 255.f blue:251.f / 255.f alpha:1.0]; // iOS7 Safari bar color
         if ([UIApplication.sharedApplication.delegate.window respondsToSelector:@selector(setTintColor:)] && UIApplication.sharedApplication.delegate.window.tintColor) {
             tintColor = UIApplication.sharedApplication.delegate.window.tintColor;
